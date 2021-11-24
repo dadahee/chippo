49c9ad46447b10972e92f76b15138c47f12c183a
turtle601 <turtle601@naver.com>
2021-11-24 10:17:25 +0900

    feat: react-router-dom 설치 및 세팅

--- front-end/src/components/app.jsx
+++ front-end/src/components/app.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { 
+    BrowserRouter as Router, 
+    Routes,
+    Route, 
+} from 'react-router-dom';
+
+import Home from './home.jsx';
+
+// 1. router 다운로드 및 세팅 (0)
+// 2. 차크라 ui 다운로드 
+// 3. 헤더 작성 구현
+// 4. 다크모드 구현 ?
+// 5. nav 구현
+// 6. 사진 레이아웃 구성 (table)
 
 function App(){
-    return <h1>취뽀입니다</h1>
-}
+    return (
+        <Router>
+            {/* 헤더 작성 */}
+            <div>
+                <Routes>
+                    <Route path = "/" element = {<Home />} />
+                </Routes>
+            </div>
+        </Router>   
+    );
+};
 
 export default App;
