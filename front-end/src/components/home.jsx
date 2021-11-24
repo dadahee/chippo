af37f2ca3b28d5c93c75390dfbf162427aa84fac
turtle601 <turtle601@naver.com>
2021-11-24 10:34:13 +0900

    chores: 차크라 ui 다운로드 #9

--- front-end/src/components/home.jsx
+++ front-end/src/components/home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Button } from "@chakra-ui/react"
+
 
 function Home(){
-    return <div>홈입니다.</div>
+    return (
+        <>
+            <div>홈입니다.</div>
+            <Button>눌러주세여!!</Button>
+        </>
+    )
 }
 
 export default Home;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+
 import App from './components/app.jsx';
 
 ReactDOM.render(
-    <App />,
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>, 
   document.getElementById('root')
 );
 
