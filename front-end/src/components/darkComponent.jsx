4c67c0377de91859471ccc96cfe1ca85aaa27da9
turtle601 <turtle601@naver.com>
2021-11-24 16:41:26 +0900

    [Front] feat: 다크 모드 컴포넌트 구현 #11

--- front-end/src/components/darkComponent.jsx
+++ front-end/src/components/darkComponent.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+
 import { IconButton } from "@chakra-ui/button";
-// import { FaSun, FaMoon } from "react-icons/fa"
+import { useColorMode } from '@chakra-ui/color-mode';
+import { FaSun, FaMoon } from "react-icons/fa"
+import { Flex, Spacer } from '@chakra-ui/layout';
 
 function DarkModeComponent(){
+    const { colorMode, toggleColorMode } = useColorMode();
+    
+    const isDark = colorMode === "dark";
+
     return (
-        <div>다크모드
-        </div>        
+        <Flex p = {8}>
+            <Spacer />
+            <IconButton 
+                w = "48px"
+                h = "48px"
+                icon = {isDark ? <FaSun /> : <FaMoon />}
+                isRound = "true"
+                onClick = {toggleColorMode}
+            >
+            </IconButton>
+        </Flex>        
     )
 }
 
