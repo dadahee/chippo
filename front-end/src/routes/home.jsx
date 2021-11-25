d4e2d4ae12d2a9a434939e12f3d2230ea474a25d
turtle601 <turtle601@naver.com>
2021-11-25 11:31:36 +0900

    [Front] style: tab바 구현- 고차함수를 통해 코드 양 줄임 #12

--- front-end/src/routes/home.jsx
+++ front-end/src/routes/home.jsx
@@ -1,60 +1,56 @@
 import React, { useEffect, useState } from 'react';
 
 import { Box, Center, SimpleGrid } from '@chakra-ui/layout';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Image } from "@chakra-ui/react"
 
 import { Section } from '../components/theme/common.style.js';
 
 function Home(){
-    // App 전체에서 state설정, redux로 관리 
-    useEffect(() => {},[])
+    const tabList = ["기업별", "스택별", "직무별"];
+    // const [imgJson, setimgJson] = useState({});
+
+    useEffect(() => {
+        // setimgJson(homeTabDummy[0]);
+    })
 
     return (
         <Section >
-            <HomeTab />
+            <Center>
+                <Tabs w= "80%" p = {4}>
+                <TabList 
+                    w = "100%" 
+                    h = "50px" 
+                    bgGradient = "linear(to-r, #E6F0FF, #5078E7)" >
+                    {
+                        tabList.map((tab) => {
+                            return <Tab w = "100px">{tab}</Tab>
+                        })
+                    }
+                </TabList>
+                <TabPanels bg = "white">
+                    {
+                        tabList.map((tab) => {
+                            return <TabPanel><p>{tab}</p></TabPanel>
+                        })
+                    }
+                </TabPanels>
+                </Tabs>
+            </Center>
         </Section>
     )
 }
 
-function HomeTab(){
-    return (
-        <Center>
-            <Tabs w= "80%" p = {4}>
-            <TabList
-                w = "100%" 
-                h = "50px" 
-                bgGradient = "linear(to-r, #E6F0FF, #5078E7)" 
-            >
-                <Tab w = "100px">기업별</Tab>
-                <Tab w = "100px">스택별</Tab>
-                <Tab w = "100px">직무별</Tab>
-            </TabList>
-
-            <TabPanels>
-                <TabPanel>
-                 <HomeTabPanel />
-                </TabPanel>
-                <TabPanel>
-                <p>two!</p>
-                </TabPanel>
-                <TabPanel>
-                <p>three!</p>
-                </TabPanel>
-            </TabPanels>
-            </Tabs>
-            
-        </Center>
-    )
-}
-
 function HomeTabPanel(){
+    
     return (
-        <SimpleGrid columns = {3} spacing = {10} minChildWidth = "30%">
-            <Box bg = "tomato" height = "80px" />
-            <Box bg = "tomato" height = "80px" />
-            <Box bg = "tomato" height = "80px" />
-            <Box bg = "tomato" height = "80px" />
-            <Box bg = "tomato" height = "80px" />
+        <SimpleGrid columns = {3} spacing = {8} minChildWidth = "30%">
+            {/* <Image 
+                objectFit="contain" 
+                htmlHeight = "200px"
+                htmlWidth = "300px"
+                src= {process.env.PUBLIC_URL + kakao.카카오이미지} 
+                alt="chippo_logo" 
+            /> */}
 
         </SimpleGrid>
     )
