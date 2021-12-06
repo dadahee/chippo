2bf7eb8ed4730f902826bf2c2690434e7dd29482
turtle601 <turtle601@naver.com>
2021-12-06 21:48:37 +0900

    [chores] redux-thunk 설치 및 셋팅 #39

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,11 +1,12 @@
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from './rootReducer.js';
 
-const middleware = [logger];
+const middleware = [logger, thunk];
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
