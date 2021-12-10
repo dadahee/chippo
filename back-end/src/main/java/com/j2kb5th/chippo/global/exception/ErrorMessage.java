417a7a91386fae2c6caef1f9aa691231a08af8cd
Dahee Goo <heeouo@kookmin.ac.kr>
2021-12-10 17:38:18 +0900

    chore: 불필요한 import 구문, 주석 삭제, ErrorMessage에 글로벌 타입 에러 추가 - 전반적인 포맷팅 수정 - 목록 조회 구현중

--- back-end/src/main/java/com/j2kb5th/chippo/global/exception/ErrorMessage.java
+++ back-end/src/main/java/com/j2kb5th/chippo/global/exception/ErrorMessage.java
@@ -6,6 +6,12 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     /**
+     * GL### : Global Error
+     */
+    GL001("유저 정보를 찾을 수 없습니다."),
+
+
+    /**
      * AD### : Admin Error
      */
     AD001("에러 메시지 내용을 작성"),
