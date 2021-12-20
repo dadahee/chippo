f9b9bb42753721a3ac85798a447a573000ae4e08
turtle601 <turtle601@naver.com>
2021-12-20 10:29:48 +0900

    chores: form 작성을 도와주는 formik설치 #65

--- front-end/src/pages/writing.jsx
+++ front-end/src/pages/writing.jsx
@@ -1,18 +1,66 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 import { useNavigate } from 'react-router-dom';
 
 import { connect } from "react-redux";
+
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    Button,
+    Container
+  } from '@chakra-ui/react'
 
 function Writing({ logined }){
 
     const navigator = useNavigate();
     
     if (logined === false) navigator("/login");
-
-    return (
-        <div>새글작성 페이지</div>
-    )
+    
+      return (
+          <FormikExample />
+      )
+    
 }
+
+function FormikExample() {
+    function validateName(value) {
+        let error
+        if (!value) error = 'Name is required'
+        return error
+    }
+  
+    return (
+      <Formik
+        initialValues={{ name: 'Sasuke' }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            actions.setSubmitting(false)
+          }, 1000)
+        }}
+      >
+        {(props) => (
+          <Form>
+            <Field name='name' validate={validateName}>
+              {({ field, form }) => (
+                <FormControl isInvalid={form.errors.name && form.touched.name}>
+                  <FormLabel htmlFor='name'>First name</FormLabel>
+                  <Input {...field} id='name' placeholder='name' />
+                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Button variant="primary">업로드 하기</Button>
+          </Form>
+        )}
+      </Formik>
+    )
+  }
 
 const mapStateToProps = ({ logined }) => {
     return {
