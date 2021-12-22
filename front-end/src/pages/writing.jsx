2e9a2074bfeb5141ef918917d6f603ce51917046
turtle601 <turtle601@naver.com>
2021-12-22 14:31:21 +0900

    feat : input 커스터마이징 + 새글 작성 페이지 레이아웃 구현

--- front-end/src/pages/writing.jsx
+++ front-end/src/pages/writing.jsx
@@ -14,14 +14,12 @@ import {
     Center,
     VStack,
     Box,
+    Spacer,
+    Flex,
     Select,
     Textarea,
+    HStack,
   } from '@chakra-ui/react'
-
-import {
-    SelectControl,
-    TextareaControl
-} from "formik-chakra-ui";
 
 function Writing({ logined }){
 
@@ -36,18 +34,28 @@ function Writing({ logined }){
 
     const checkEnglish = (value) => {
         let error;
-        if (!/^[A-Z|a-z]+$/){
+        if (!/^[A-Z|a-z|없음]+$/.test(value)){
             error = "영어로 입력하세요"
         }
 
         return error;
     }
 
+    const checkLength = (value) => {
+        let error;
+        if (value.length === 0){
+            error = "내용을 작성하세요"
+        }
+
+        return error;
+    } 
+
     const navigator = useNavigate();
     
     if (logined === false) navigator("/login");
     
     return (
+        <>
         <Center>
         <VStack w ="70%">
             <Box w = "100%">
@@ -75,10 +83,12 @@ function Writing({ logined }){
                         name="question"
                         render={({ field, form: { isSubmitting } }) => (
                             <FormControl>
-                            <FormLabel htmlFor="question">면접 질문</FormLabel>
-                            <Input {...field} disabled={isSubmitting} 
-                                type="text" placeholder="30자 이내로 입력하세요" maxLength="40" 
-                                colorScheme="#5078E7" />
+                                <Flex>
+                                <FormLabel htmlFor="question" w = "100px" pt = "8px">면접 질문</FormLabel>
+                                <Input {...field} disabled={isSubmitting} w = "300px" variant= "primary"
+                                    type="text" placeholder="40자 이내로 입력하세요" maxLength="40" />
+                                </Flex>
+                            
                             </FormControl> 
                         )}
                     />
@@ -87,27 +97,43 @@ function Writing({ logined }){
                         name="company" validate = {checkKorean}
                         render={({ field, form : { isSubmitting, errors, touched } }) => (
                             <FormControl isInvalid = {errors.company && touched.company}>
-                                <FormLabel htmlFor="company">기업명</FormLabel>
-                                <Input {...field} disabled={isSubmitting} 
-                                    type="text" placeholder="한글로 작성 ex) 카카오, 네이버" />
-                                <FormErrorMessage>{errors.company}</FormErrorMessage>
+                                <Flex>
+                                    <FormLabel htmlFor="company" w = "100px" pt = "8px">기업명</FormLabel>
+                                    <Input {...field} disabled={isSubmitting} w = "300px" variant = "primary"
+                                        type="text" placeholder="한글로 작성 ex) 카카오, 네이버" />
+                                    <FormErrorMessage>{errors.company}</FormErrorMessage>
+                                </Flex>
+                                
+                                
                             </FormControl>
                         )}
                     />
 
+                    <HStack w = "100%" spacing={20}>
+                    <FormLabel htmlFor="stack" w = "120px" pt = "8px"> 스택 </FormLabel>
+                    
                     <Field
                         name="stack1" validate = {checkEnglish}
                         render={({ field, form : { isSubmitting, errors, touched } }) => (
+                            
                             <FormControl isInvalid = {errors.stack1 && touched.stack1}>
-                                <FormLabel htmlFor="stack1">스택1</FormLabel>
-                                <Input {...field} disabled={isSubmitting} 
+                                
+                                    <Input {...field} disabled={isSubmitting} ml = "-10px"
+                                    variant="primary"
                                     type="text" name = "stack1" list = "stack1" />
-                                <datalist id="stack1">
-                                    <option value="컴퓨터공학과"></option>
-                                    <option value="영어영문과"></option>
-                                    <option value="경영학과"></option>
-                                    <option value="사회체육과"></option>
-                                </datalist>
+                                    <datalist id="stack1">
+                                        <option value="python"></option>
+                                        <option value="javascript"></option>
+                                        <option value="java"></option>
+                                        <option value="php"></option>
+                                        <option value="go"></option>
+                                        <option value="c"></option>
+                                        <option value="c#"></option>
+                                        <option value="c++"></option>
+                                        <option value="없음"></option>
+                                    </datalist>
+                                    
+                                
                                 <FormErrorMessage>{errors.stack1}</FormErrorMessage>
                             </FormControl>
                         )}
@@ -117,14 +143,19 @@ function Writing({ logined }){
                         name="stack2" validate = {checkEnglish}
                         render={({ field, form : { isSubmitting, errors, touched } }) => (
                             <FormControl isInvalid = {errors.stack2 && touched.stack2}>
-                                <FormLabel htmlFor="stack2">스택2</FormLabel>
                                 <Input {...field} disabled={isSubmitting} 
+                                    variant = "primary"
                                     type="text" name = "stack2" list = "stack2" />
                                 <datalist id="stack2">
-                                    <option value="컴퓨터공학과"></option>
-                                    <option value="영어영문과"></option>
-                                    <option value="경영학과"></option>
-                                    <option value="사회체육과"></option>
+                                    <option value="python"></option>
+                                    <option value="javascript"></option>
+                                    <option value="java"></option>
+                                    <option value="php"></option>
+                                    <option value="go"></option>
+                                    <option value="c"></option>
+                                    <option value="c#"></option>
+                                    <option value="c++"></option>
+                                    <option value="없음"></option>
                                 </datalist>
                                 <FormErrorMessage>{errors.stack2}</FormErrorMessage>
                             </FormControl>
@@ -134,52 +165,66 @@ function Writing({ logined }){
                     <Field
                         name="stack3" validate = {checkEnglish}
                         render={({ field, form : { isSubmitting, errors, touched } }) => (
-                            <FormControl isInvalid = {errors.stack1 && touched.stack1}>
-                                <FormLabel htmlFor="stack3">스택3</FormLabel>
+                            <FormControl isInvalid = {errors.stack3 && touched.stack3}>
                                 <Input {...field} disabled={isSubmitting} 
+                                    variant = "primary"
                                     type="text" name = "stack3" list = "stack3" />
                                 <datalist id="stack3">
-                                    <option value="컴퓨터공학과"></option>
-                                    <option value="영어영문과"></option>
-                                    <option value="경영학과"></option>
-                                    <option value="사회체육과"></option>
+                                    <option value="python"></option>
+                                    <option value="javascript"></option>
+                                    <option value="java"></option>
+                                    <option value="php"></option>
+                                    <option value="go"></option>
+                                    <option value="c"></option>
+                                    <option value="c#"></option>
+                                    <option value="c++"></option>
+                                    <option value="없음"></option>
                                 </datalist>
                                 <FormErrorMessage>{errors.stack3}</FormErrorMessage>
                             </FormControl>
                         )}
                     />
 
+
+
+                    </HStack>
                     <Field
                         name="job" validate = {checkEnglish}
                         render={({ field, form : { isSubmitting, errors, touched } }) => (
                             <FormControl isInvalid = {errors.job && touched.job}>
-                                <FormLabel htmlFor="job">직무</FormLabel>
+                                <Flex>
+                                <FormLabel htmlFor="job" w = "100px" pt = "8px">직무</FormLabel>
                                 <Input {...field} disabled={isSubmitting} w = "300px"
-                                    type="text" name = "job" list = "job" />
+                                    type="text" name = "job" list = "job"
+                                    variant= "primary" />
                                 <datalist id="job">
-                                    <option value="컴퓨터공학과"></option>
-                                    <option value="영어영문과"></option>
-                                    <option value="경영학과"></option>
-                                    <option value="사회체육과"></option>
+                                    <option value="backend"></option>
+                                    <option value="frontend"></option>
+                                    <option value="data-engineer"></option>
+                                    <option value="ios"></option>
+                                    <option value="android"></option>
+                                    <option value="없음"></option>
                                 </datalist>
                                 <FormErrorMessage>{errors.job}</FormErrorMessage>
+                                </Flex>
                             </FormControl>
                         )}
                     />
 
                     <Field
-                        name="answer"
+                        name="answer" validate = {checkLength}
                         render={({ field, form : { isSubmitting, errors, touched } }) => (
                             <FormControl isInvalid = {errors.answer && touched.answer}>
                                 <Textarea {...field} disabled={isSubmitting} 
-                                    type="text" name = "answer" placeholder='내가 생각하는 질문에 대한 답변을 적어주세요!!' />
+                                    h = "200px" minLength = "300"
+                                    type="text" name = "answer" placeholder='내가 생각하는 질문에 대한 답변을 300자 이내에 적어주세요!!' />
                                 <FormErrorMessage>{errors.answer}</FormErrorMessage>
                             </FormControl>
                         )}
                     />
 
                     <Field
-                        name="additionalInformation" validate = {checkEnglish}
+                        name="additionalInformation"
                         render={({ field, form : { isSubmitting, errors, touched } }) => (
                             <FormControl isInvalid = {errors.additionalInformation && touched.additionalInformation}>
                                 <Textarea {...field} disabled={isSubmitting} 
@@ -190,18 +235,21 @@ function Writing({ logined }){
                     />
                     
 
+                    <Flex p ={8}>
+                        <Spacer />
+                        <Button variant="primary" type="submit">업로드 하기</Button>
+                    </Flex>
                     </VStack>
-                    <Button variant="primary" type="submit">업로드 하기</Button>
+                    
                 </Form>
                 </Formik>
             </Box>
-            
-            
-                    
-            
-            
         </VStack>
+        
+        
         </Center>
+        
+        </>
         
     )
     
