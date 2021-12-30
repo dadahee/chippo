25527dfcd78bcc46dd77f004b9f8b2857c8197c4
turtle601 <turtle601@naver.com>
2021-12-30 17:12:28 +0900

    fix : 데이터 양식 수정

--- front-end/src/data.js
+++ front-end/src/data.js
@@ -112,7 +112,7 @@ const naverInterviewLists = {
                 id : 0, 
                 nickname : "정재원"
             },
-            question : "Q. Call by value, Call by reference의 차이에 대해 설명해 보세요.",
+            question : "Call by value, Call by reference의 차이에 대해 설명해 보세요.",
             interviewTags : [
                 {
                     id : 0,
@@ -139,8 +139,10 @@ const homeTags = [
     }
 ]
 
-const idInterview = {
-    "id": 0,
+const idInterview = [
+    {},{},{},{},
+    {
+    "id": 4,
     "user": {
       "id": 0,
       "nickname": "구희승"
@@ -180,7 +182,92 @@ const idInterview = {
       }
     ],
     "updatedAt": "2021-12-13T13:43:32.050Z"
-  }
+  },
+  {
+    "id": 5,
+    "user": {
+      "id": 0,
+      "nickname": "구다희"
+    },
+    "question": "리버스 프락시란 무엇인가요?",
+    "answer": "Reverse Proxy는 클라이언트로부터의 요청을 받아서 적절한 웹 서버로 요청을 전송하는 도중의 처리에 끼어들어서 다양한 전후처리를 시행하는 역할을 합니다.",
+    "extraInfo": "",
+    "preAnswer": {
+      "id": 0,
+      "content": "string",
+      "user": {
+        "id": 0,
+        "nickname": "string"
+      },
+      "updatedAt": "2021-12-13T13:43:32.050Z"
+    },
+    "thumb": {
+      "clicked": true,
+      "count": 0
+    },
+    "interviewTags": [
+      {
+        "id": 0,
+        "type": "TECHSTACK",
+        "name": "string"
+      }
+    ],
+    "comments": [
+      {
+        "id": 0,
+        "user": {
+          "id": 0,
+          "nickname": "황준승"
+        },
+        "content": "간략하게 정리를 잘하셨군요!!",
+        "updatedAt": "2021-12-13T13:43:32.050Z"
+      }
+    ],
+    "updatedAt": "2021-12-13T13:43:32.050Z"
+  },
+  {
+    "id": 5,
+    "user": {
+      "id": 0,
+      "nickname": "정재원"
+    },
+    "question": "Call by value, Call by reference의 차이에 대해 설명해 보세요.",
+    "answer": "간단히 말해 값을 복사를 하여 처리를 하느냐, 아니면 직접 참조를 하느냐 차이입니다. Call by value는 인자로 받은 값을 복사하여 처리를 합니다. 복사된 많은 계산이 들어간다면 과부하의 원인이 될 수 있습니다. 하지만 원래의 값은 영향을 받지 않아서 안전합니다. Call by reference는 인자로 받은 값의 주소를 참조하여 직접 값에 영향을 줍니다. 직접 참조이기에 빠르지만 원래 값이 영향을 받아 변경되는 리스크가 있습니다. ",
+    "extraInfo": "",
+    "preAnswer": {
+      "id": 0,
+      "content": "string",
+      "user": {
+        "id": 0,
+        "nickname": "string"
+      },
+      "updatedAt": "2021-12-13T13:43:32.050Z"
+    },
+    "thumb": {
+      "clicked": true,
+      "count": 0
+    },
+    "interviewTags": [
+      {
+        "id": 0,
+        "type": "TECHSTACK",
+        "name": "string"
+      }
+    ],
+    "comments": [
+      {
+        "id": 0,
+        "user": {
+          "id": 0,
+          "nickname": "황준승"
+        },
+        "content": "간략하게 정리를 잘하셨군요!!",
+        "updatedAt": "2021-12-13T13:43:32.050Z"
+      }
+    ],
+    "updatedAt": "2021-12-13T13:43:32.050Z"
+  },
+]
 
 export {
     homeTags,
